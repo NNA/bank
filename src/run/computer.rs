1a1d2319bca4e84ee9930603276cad00ead1b490
Nicolas NARDONE <nna@protonmail.com>
2021-11-19 23:19:36 +0100

    Wip processing deposit

--- src/run/computer.rs
+++ src/run/computer.rs
@@ -1,38 +1,76 @@
+use crate::run::tx::AccountBalance;
+use crate::run::tx::BalanceBook;
+use crate::run::tx::Deposit;
 use crate::run::tx::RawTransaction;
 use crate::run::tx::RawTransactionsList;
-use log::debug;
+use crate::run::tx::TransactionKind;
+use log::{debug, warn};
 
-pub fn compute_balance(txs: RawTransactionsList) {
+pub fn compute_balance(txs: RawTransactionsList) -> BalanceBook {
+    let mut b_book: BalanceBook = BalanceBook::new();
     for tx in txs {
-        // let res = check_transaction(tx);
-        // debug!(res);
-        // match res {
-        //     Some(expr) => expr,
-        //     None => expr,
-        // }
+        // let boxed_tx: Box<_>;
+        match tx.kind {
+            Some(ref kind) => match kind {
+                TransactionKind::Deposit => match Deposit::try_from(tx) {
+                    Ok(deposit) => {
+                        if let Some(account) = b_book.get_mut(&deposit.account_id) {
+                            account.process_deposit(deposit);
+                        } else {
+                            let id = deposit.account_id;
+                            let ab = AccountBalance::new();
+                            ab.process_deposit(deposit);
+                            b_book.insert(id, ab);
+                        };
+                    }
+                    Err(e) => warn!("Invalid deposit : {:?}", e),
+                },
+                _ => (),
+            },
+            None => warn!(
+                "Unknown transaction {:?}. Skipping this transaction",
+                tx.kind
+            ),
+        }
+    }
+    b_book
+}
+
+impl AccountBalance {
+    pub fn new() -> Self {
+        AccountBalance {
+            ..Default::default()
+        }
+    }
+    pub fn process_deposit(&self, deposit: Deposit) {
+        debug!("processing deposit {:?}", deposit);
     }
 }
 
-// fn check_transaction(tx: RawTransaction) -> Result<(), std::result::Result> {
-//     unimplemented!()
-// }
+impl Default for AccountBalance {
+    fn default() -> Self {
+        AccountBalance {
+            locked: false,
+            available: 0,
+            held: 0,
+        }
+    }
+}
 
-// impl From for Type {
-// 	// add code here
-// }
+impl TryFrom<RawTransaction> for Deposit {
+    type Error = String;
 
-// impl TryFrom<RawTransaction> for Transaction {
-//     type Error = String;
-
-//     fn try_from(tx: RawTransaction) -> Result<Self, Self::Error> {
-//         match s.to_lowercase().as_str() {
-//             "local" => Ok(Self::Local),
-//             "ci" => Ok(Self::Ci),
-//             "production" => Ok(Self::Production),
-//             other => Err(format!(
-//                 "{} is not a supported environment. Use either `local` or `ci` or `production`.",
-//                 other
-//             )),
-//         }
-//     }
-// }
+    fn try_from(raw_tx: RawTransaction) -> Result<Self, Self::Error> {
+        if let None = raw_tx.client {
+            return Err("A Deposit must have a client".to_string());
+        }
+        if let None = raw_tx.tx {
+            return Err("A Deposit must have a valid transaction id".to_string());
+        }
+        Ok(Deposit {
+            id: raw_tx.tx.unwrap(),
+            account_id: raw_tx.client.unwrap(),
+            amount: 10,
+        })
+    }
+}
--- src/run/mod.rs
+++ src/run/mod.rs
@@ -3,11 +3,17 @@ pub mod config;
 pub mod parser;
 pub mod tx;
 
+use crate::run::tx::BalanceBook;
+use log::info;
 use std::error::Error;
 use tx::RawTransactionsList;
 
 pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
-    let _result: RawTransactionsList = parser::parse_transactions_file(&config.tx_file)?;
+    let result: RawTransactionsList = parser::parse_transactions_file(&config.tx_file)?;
+    info!("after parsing result is {:?}", result);
+
+    let balance: BalanceBook = computer::compute_balance(result);
+    info!("after compute_balance balance is {:?}", balance);
 
     Ok(())
 }
