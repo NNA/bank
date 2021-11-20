4270b65e102ce5ab90270e135c7a09d0c8c00a63
Nicolas NARDONE <nna@protonmail.com>
2021-11-20 12:32:43 +0100

    Add deposit creation test

--- src/models/tx.rs
+++ src/models/tx.rs
@@ -1,6 +1,8 @@
 use crate::models::raw_tx::RawTransaction;
 use crate::models::AccountId;
-use crate::models::Decimal;
+use rust_decimal::prelude::*;
+use rust_decimal::Decimal;
+// use crate::models::Decimal;
 
 type TxId = u32;
 
@@ -26,12 +28,44 @@ impl TryFrom<RawTransaction> for Deposit {
             return Err("A Deposit must have a client".to_string());
         }
         if let None = raw_tx.tx {
-            return Err("A Deposit must have a valid transaction id".to_string());
+            return Err("A Deposit must have a transaction id".to_string());
+        }
+        if let None = raw_tx.amount {
+            return Err("A Deposit must have an amount".to_string());
+        }
+        let mut am: Decimal;
+        match Decimal::from_str(&raw_tx.amount.unwrap()) {
+            Ok(a) => am = a,
+            Err(e) => return Err("Invalid amount".to_string()),
         }
         Ok(Deposit {
             id: raw_tx.tx.unwrap(),
             account_id: raw_tx.client.unwrap(),
-            amount: 10,
+            amount: am,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::models::raw_tx::TransactionKind;
+
+    #[test]
+    fn deposit_can_be_created_from_raw_transaction() {
+        // Prepare data
+        let raw_tx = RawTransaction {
+            kind: Some(TransactionKind::Deposit),
+            client: Some(1),
+            tx: Some(42),
+            amount: Some("23.2".to_string()),
+        };
+
+        let res = Deposit::try_from(raw_tx);
+        assert!(res.is_ok());
+        let deposit: Deposit = res.unwrap();
+        assert_eq!(deposit.id, 42);
+        assert_eq!(deposit.account_id, 1);
+        assert_eq!(deposit.amount, Decimal::new(232_000, 4));
     }
 }
